d3914292996f7eb5fcd7829e2f1460cf9733e9a8
Ryan Fleck <ryan.fleck@protonmail.com>
2020-07-23 07:47:58 -0400

    Add prerequisites for publishing.

--- README.md
+++ README.md
@@ -2,4 +2,29 @@
 
 This is a work in progress component library.
 
+
+
+### Development
+
+```sh
+# Install packages
+yarn install
+
+# Run styleguidist
+yarn start
+
+# Run testing React app
+yarn react-start
+```
+
 Use `npx styleguidist server` to run the app. 
+
+
+### Publishing
+
+Contributors must request access to the `@xalgorithms` npm organization to publish the package.
+
+```sh
+npm login
+npm publish --access public
+```
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require("path");
+module.exports = {
+  mode: "production",
+  entry: "./index.js",
+  output: {
+    path: path.resolve("./"),
+    filename: "index.js",
+    libraryTarget: "commonjs2",
+  },
+  resolve: {
+    alias: {
+      react: path.resolve("./node_modules/react"),
+      "react-dom": path.resolve("./node_modules/react-dom"),
+    },
+  },
+  externals: {
+    react: {
+      commonjs: "react",
+      commonjs2: "react",
+      amd: "React",
+      root: "React",
+    },
+    "react-dom": {
+      commonjs: "react-dom",
+      commonjs2: "react-dom",
+      amd: "ReactDOM",
+      root: "ReactDOM",
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        use: "babel-loader",
+      }
+    ],
+  },
+};
