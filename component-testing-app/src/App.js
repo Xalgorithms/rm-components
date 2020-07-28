396858a26b0f463d9b776e53c806a7e9e867a2b1
Ryan Fleck <ryan.fleck@protonmail.com>
2020-07-28 15:53:12 -0400

    Add testing application.

--- component-testing-app/src/App.js
+++ component-testing-app/src/App.js
@@ -1,8 +1,6 @@
 import React from 'react';
 import logo from './logo.svg';
-import { ThemeProvider } from 'styled-components'
-import theme from './theme'
-
+import './App.css';
 
 function App() {
   return (
