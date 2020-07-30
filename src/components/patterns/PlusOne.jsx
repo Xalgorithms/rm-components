939071e107a4d4bf11c4b20db18e963854e2ce0e
Ryan Fleck <ryan.fleck@protonmail.com>
2020-07-30 09:35:54 -0400

    Update PlusOne

--- src/components/patterns/PlusOne.jsx
+++ src/components/patterns/PlusOne.jsx
@@ -2,34 +2,23 @@ import React, { useState, useEffect } from 'react';
 import { Box, Button, Text, InvolvedParty } from '..';
 
 function PlusOne() {
-  const numbers = [1];
-  const [count, setCount] = useState(1);
+  const [count, setCount] = useState(0);
+  const message = `You clicked ${count} times.`;
 
   const plus = () => {
     setCount(count + 1);
     console.log(count);
   };
 
-  const check = () => {
-    numbers.push(count);
-    console.log(numbers);
-    listComp();
-    // console.log(numbers);
-  };
-
-  const listComp = () => {
-    return numbers.map((index) => <li key={index}>:)</li>);
-  };
-
   useEffect(() => {
     // Update the document title using the browser API
-    document.title = `You clicked ${listComp()} times.`;
+    document.title = message;
   });
 
   return (
     <Box>
-      <Text id="morebox">You clicked{listComp()} times.</Text>
-      <Button onClick={(plus, check)}>Click me</Button>
+      <Text id="morebox">{message}</Text>
+      <Button onClick={plus}>Click me</Button>
     </Box>
   );
 }
